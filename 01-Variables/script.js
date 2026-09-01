f6542796fe4cb6e84347e61c1f787e56c3facb42
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-01 16:14:17 +0530

    work variables

---
+++ 01-Variables/script.js
@@ -0,0 +1,29 @@
+const accountId = 14436
+
+let accountEmail = "Ayushsharma@123" 
+
+var idPassword = "1233"
+
+accountCity = "chandighr"
+
+// accountId = 2 // not allowed*
+
+accountEmail =  "htttp.com"
+
+idPassword = "234455555"
+
+accountCity = "nagota"
+
+console.log(accountId);
+
+// \**
+
+// prefer not to use var*
+
+// because of issue in block scope and funntion scope*
+
+//   \**
+
+
+
+console.table([accountId, accountEmail, idPassword, accountCity]);
