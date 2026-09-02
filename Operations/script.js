85d542fa5933d59f8cc6029ee17d3de8a7cb038a
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-02 14:52:08 +0530

    operations

---
+++ Operations/script.js
@@ -0,0 +1,31 @@
+let str1= "Hello"
+let str2= " ayush"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( 3 + 4 * 5 % 3);
+// console.log(+true);
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+
+// let x = 3;
+// console.log(x++);
+// console.log(x);
+
+let x = 3;
+console.log(++x);
+console.log(x);
