f8ce8b071cb4cdb9bc739d52564fee251d15f6a6
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-02 13:26:58 +0530

    datatype

--- 02-Datatype/script.js
+++ 02-Datatype/script.js
@@ -1,26 +1,26 @@
-let score = true
+// let score = true
 
-// console.log(typeof score);
+// // console.log(typeof score);
 
-let valuInNumber = Number(score)
-// console.log(typeof valuInNumber);
-// console.log(valuInNumber);
+// let valuInNumber = Number(score)
+// // console.log(typeof valuInNumber);
+// // console.log(valuInNumber);
 
-// "33"=>33
-// "33abc" => NaN
-// true => 1; false => 0
+// // "33"=>33
+// // "33abc" => NaN
+// // true => 1; false => 0
 
-let isLoggedIn = ""
+// let isLoggedIn = ""
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn);
 
-//  1 => true; 0 => false
-// "ayush" => true
-// "" => false
+// //  1 => true; 0 => false
+// // "ayush" => true
+// // "" => false
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// // console.log(typeof stringNumber);
