2e7038a35ae45b20743822174c07cc0949666d0f
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-02 13:30:40 +0530

    Datatype

--- 02-Datatype/work.js
+++ 02-Datatype/work.js
@@ -1,106 +1,106 @@
-// 1.Create variables for your name, age, height, and student status using the correct data types.
+1.Create variables for your name, age, height, and student status using the correct data types.
 
-// let Name = "ayush"
-// let age = 22;
-// let height = 5.11;
-// let isStudent = true;
-// console.log(Name);
-// console.log(age);
-// console.log(height);
-// console.log(isStudent);
+let Name = "ayush"
+let age = 22;
+let height = 5.11;
+let isStudent = true;
+console.log(Name);
+console.log(age);
+console.log(height);
+console.log(isStudent);
 
-// 2.Create one variable each for String, Number, Boolean, Undefined, and Null.
+2.Create one variable each for String, Number, Boolean, Undefined, and Null.
 
-// let a = "Hello";
-// let b = 100;
-// let c = true;
-// let d;
-// let e = null;
+let a = "Hello";
+let b = 100;
+let c = true;
+let d;
+let e = null;
 
-// console.log(typeof a);
-// console.log(typeof b);
-// console.log(typeof c);
-// console.log(typeof d);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
 
-// 3. Create a variable with "100" and convert it from String to Number.
+3. Create a variable with "100" and convert it from String to Number.
 
-// let value = "100";
+let value = "100";
 
-// let numberValue = Number(value);
+let numberValue = Number(value);
 
-// console.log(numberValue);
-// console.log(typeof numberValue);
+console.log(numberValue);
+console.log(typeof numberValue);
 
-// 4. Create two numbers and perform addition, subtraction, multiplication, and division.
+4. Create two numbers and perform addition, subtraction, multiplication, and division.
 
-// let a = 20;
-// let b = 10;
+let a = 20;
+let b = 10;
 
-// console.log(a + b);
-// console.log(a - b);
-// console.log(a * b);
-// console.log(a / b);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
 
-// console.log(typeof (a + b));
+console.log(typeof (a + b));
 
-// 5.Create an Array containing 5 fruits and check its data type.
-// let fruits = ["Apple", "Mango", "Banana", "Orange", "Grapes"];
+5.Create an Array containing 5 fruits and check its data type.
+let fruits = ["Apple", "Mango", "Banana", "Orange", "Grapes"];
 
-// console.log(fruits);
-// console.log(typeof fruits);
+console.log(fruits);
+console.log(typeof fruits);
 
-// 6.Create an Object containing name, age, city, and isStudent.
-// let student = {
-//     Name: "ayush",
-//     age: 22,
-//     city: "chandighr",
-//     isStudent: true
-// };
+6.Create an Object containing name, age, city, and isStudent.
+let student = {
+    Name: "ayush",
+    age: 22,
+    city: "chandighr",
+    isStudent: true
+};
 
-// console.log(student);
-// console.log(typeof student);
+console.log(student);
+console.log(typeof student);
 
-// 7. Create one variable with undefined and another with null, then check their data types.
+7. Create one variable with undefined and another with null, then check their data types.
 
-// let x;
-// let y = null;
+let x;
+let y = null;
 
-// console.log(x);
-// console.log(typeof x);
+console.log(x);
+console.log(typeof x);
 
-// console.log(y);
-// console.log(typeof y);
+console.log(y);
+console.log(typeof y);
 
-// 8. Create a Boolean value and convert it into a String.
+8. Create a Boolean value and convert it into a String.
 
-// let value = true;
+let value = true;
 
-// let stringValue = String(value);
+let stringValue = String(value);
 
-// console.log(stringValue);
-// console.log(typeof stringValue);
+console.log(stringValue);
+console.log(typeof stringValue);
 
-// 9. Use typeof to check the data types of String, Number, Boolean, Undefined, Null, Array, and Object.
+9. Use typeof to check the data types of String, Number, Boolean, Undefined, Null, Array, and Object.
 
-// console.log(typeof "hello");
-// console.log(typeof 25);
-// console.log(typeof true);
-// console.log(typeof undefined);
-// console.log(typeof null);
-// console.log(typeof []);
-// console.log(typeof {});
+console.log(typeof "hello");
+console.log(typeof 25);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof []);
+console.log(typeof {});
 
-// 10. Create an Array containing different JavaScript data types and use a loop to print each value and its data type.
-// let data = [
-//     "Ayush",
-//      22,
-//     true,
-//     null,
-//     undefined,
-//     {city: "chadigarh"},
-//     ["html", "css", "js"]
-// ];
+10. Create an Array containing different JavaScript data types and use a loop to print each value and its data type.
+let data = [
+    "Ayush",
+     22,
+    true,
+    null,
+    undefined,
+    {city: "chadigarh"},
+    ["html", "css", "js"]
+];
 
-// for (let i = 0; i < data.length; i++){
-//     console.log(data[i], typeof data[i]);
-// }
+for (let i = 0; i < data.length; i++){
+    console.log(data[i], typeof data[i]);
+}
