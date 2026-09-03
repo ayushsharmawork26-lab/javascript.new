d32a160e4dd22edd6eac296eef18c303e8bdcb1c
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-03 21:27:34 +0530

    comparsion 10 task

---
+++ 04-Comparison of Datatype/work.js
@@ -0,0 +1,117 @@
+// 1. Output batao: ?
+// soluion======>
+
+// console.log(10 == "10");
+// console.log(10 == "10");
+
+// output= 1> true
+//         2> false
+
+// 2>========// (False) // (False)=====> ?
+ 
+// console.log(5 == true);
+// console.log(5 === true);
+
+// output= 1> False
+//         2> false
+
+// 3. Output batao: ?
+// solve:----------
+
+// console.log(0 == false);
+// console.log(0 === false);
+
+// output= 1> True
+//         2> False
+
+// 4. Output batao: ?
+// solve:----------->
+
+// console.log(null == undefined);
+// console.log(null === undefined);
+
+// output== 1> True
+//          2> False
+
+// 5. Output batao: ?
+// solve:---------->
+
+// console.log("5" > 3);
+// console.log("5" < 10);
+
+// output= 1> True
+//         2> True
+
+// 6. Output batao: ?
+// solve:---------->
+
+// console.log("10" == 10);
+// console.log("10" === 10);
+// console.log("10" != 10);
+// console.log("10" !== 10);
+
+// output= 1> True
+//         2> False
+//         3> False
+//         4> True
+
+// 7. Predict the output: ?
+// solve:----------->
+
+// let a = "20";
+// let b = 20;
+
+// console.log(a == b);
+// console.log(a === b);
+// console.log(a > 15);
+
+// outpt= 1> True
+//        2> False
+//        3> True
+
+// 8. Predict the output:
+// slove:------------>
+
+// console.log(true == 1);
+// console.log(false == 0);
+// console.log(true === 1);
+// console.log(false === 0);
+
+// output= 1> True
+//         2>  True
+//         3>   False
+//         4>  False
+
+// 9. Find the output: ?
+// solve:--------------->
+
+// let x = null;
+// let y = 0;
+
+// console.log(x == y);
+// console.log(x === y);
+// console.log(x != y);
+
+//  output= 1> False
+//         2>  False
+//         3>  True
+
+// 10. 🔥 Hard Task
+// Predict all outputs and explain why:
+// solve:---------->
+
+// console.log("0" == false);
+// console.log("0" === false);
+// console.log("" == false);
+// console.log("" === false);
+// console.log(" " == false);
+// console.log(null == false);
+// console.log(undefined == false);
+
+//  output= 1> True
+//         2>  False
+//         3>  True
+//         4>  False
+//         5>  True 
+//         6>  False
+//         7>  False
