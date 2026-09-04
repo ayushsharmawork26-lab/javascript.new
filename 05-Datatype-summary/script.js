61206a2795f5777f19f80d7a97fcaf5b0842fa5d
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-04 17:17:52 +0530

    Datatype 10 Task

--- 05-Datatype-summary/script.js
+++ 05-Datatype-summary/script.js
@@ -2,17 +2,17 @@
 
 //  7 type : String, Number, Boolearn, null, undefined, symbol, BigInt:
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId= Symbol('123')
+// const id = Symbol('123')
+// const anotherId= Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 23344446625252635n
 
@@ -20,14 +20,14 @@ console.log(id === anotherId);
 
 //  3 Type: Array, objects, Functions ?
 
-const heros = ["ayushman", "rahulmam", "pinkuman"];
-let myobj ={
-    Name: "ayush",
-    age: 22,
-};
+// const heros = ["ayushman", "rahulmam", "pinkuman"];
+// let myobj ={
+//     Name: "ayush",
+//     age: 22,
+// };
 
-const myFunction = function(){
-    console.log("Hello World")
-}
+// const myFunction = function(){
+//     console.log("Hello World")
+// }
 
-console.log(typeof heros);
+// console.log(typeof heros);
---
+++ 05-Datatype-summary/work.js
@@ -0,0 +1,118 @@
+// 1. ----------- String --------->
+// Solve ===>
+// let Name = "ayush";
+
+//  console.log(typeof Name);
+
+//  output: String;
+
+// 2. ------------- Number ---------->
+// solve =====>
+// let age = 22;
+// let marks = 85.5;
+
+// console.log(typeof age);
+// console.log(typeof marks);
+
+// output: 1> Number
+//         2> Number
+
+//  3. ---------- Boolean ------------>
+// solve ======> 
+// let isLoggedIn = true;
+// console.log(typeof isLoggedIn);
+
+// output : -- Boolean
+
+//  4. ----------- Undefined ----------->
+// solve =====>
+// let x;
+//  console.log(typeof x);
+
+//  Output: undefined
+
+// 5. --------------- Null ------------->
+// solve ====>
+// let value = null;
+// console.log(typeof value); 
+
+// Output: object
+
+// 6. -------------- BigInt ------------->
+// solve =====>
+// let bigNumber = 255411155411255541111n;
+
+//  console.log(typeof bigNumber);
+
+// output: BigInt
+
+// 7. ---------------- Array -------------->
+// solve ====>
+// let fruits = ["Apple", "Mango", "Banana"];
+
+// console.log(typeof fruits);
+
+// output: object
+
+// 8. -------------- Object ---------------->
+// solve ====>
+// let student = {
+//     Name: "Ayush",
+//     age: 22,
+//     course: "MCA"
+// };
+
+// console.log(typeof student);
+
+// output: object
+
+// 9 --------------- All Data Type ------------->
+// solve====>
+// console.log(typeof "Hello");    // output: string
+// console.log(typeof 100);        // output: number
+// console.log(typeof 10.5);       // output: number
+// console.log(typeof true);       // output: boolean
+// console.log(typeof undefined);  // output: undefined
+// console.log(typeof null);       // output: object
+// console.log(typeof 123n);       // output: bigint
+// console.log(typeof {});         // output: object
+// console.log(typeof []);         // output: object
+
+// 10. ---------------- Challenge Solution 🔥 ---------->
+// let Name = "Ayush";
+// let age = 22;
+// let student = true;
+// let x;
+// let value = null;
+// let bigNumber = 555741255444555n;
+// let fruits = ["Apple", "Mango"];
+// let person = {
+//     Name: "Ayush",
+//     age: 22
+// };
+
+// console.log(typeof Name);    
+// console.log(typeof age);
+// console.log(typeof student);
+// console.log(typeof x);
+// console.log(typeof value);
+// console.log(typeof bigNumber);
+// console.log(typeof fruits);
+// console.log(typeof person);
+
+// output 1> string
+// output 2> Number
+// output 3> Boolean
+// output 4> undefined
+// output 5>  object
+// output 6>   bigint
+// output 7>   object
+// output 8>   object
+
+
+
+
+
+
+
+
