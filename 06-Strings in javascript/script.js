186c5daf97e8133729045a607542ca944db3a378
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-06 15:18:22 +0530

    Add javascript string methods

---
+++ 06-Strings in javascript/script.js
@@ -0,0 +1,26 @@
+const Name = "Ayush"
+const repoCount = 50;
+
+// console.log(Name + repoCount + " value");
+
+console.log(`Hello my Name is ${Name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Ayushsharmaasa-as')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-6, 4)
+console.log(anotherString);
+
+const newStringOne = "  Ayush  "
+console.log(newStringOne.trim());
