cc95a99145b1e67428385555f24724054c1c5059
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-06 15:39:29 +0530

    javascript string

--- 06-Strings in javascript/script.js
+++ 06-Strings in javascript/script.js
@@ -1,26 +1,33 @@
-const Name = "Ayush"
-const repoCount = 50;
+// const Name = "Ayush"
+// const repoCount = 50;
 
-// console.log(Name + repoCount + " value");
+// // console.log(Name + repoCount + " value");
 
-console.log(`Hello my Name is ${Name} and my repo count is ${repoCount}`);
+// console.log(`Hello my Name is ${Name} and my repo count is ${repoCount}`);
 
-const gameName = new String('Ayushsharmaasa-as')
+// const gameName = new String('Ayushsharmaasa-as')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+// // console.log(gameName.length);
+// // console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
 
-const anotherString = gameName.slice(-6, 4)
-console.log(anotherString);
+// const anotherString = gameName.slice(-6, 4)
+// console.log(anotherString);
 
-const newStringOne = "  Ayush  "
-console.log(newStringOne.trim());
+// const newStringOne = "  Ayush  "
+// console.log(newStringOne.trim());
+
+
+// const url = "https://ayush.com/ayush%20sharma"
+
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('sundar'));
