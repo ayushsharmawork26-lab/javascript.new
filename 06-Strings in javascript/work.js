0188338d8caf33c665fde8b63fc4247b628ad9ee
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-06 16:42:32 +0530

    strings 10 Tasks

---
+++ 06-Strings in javascript/work.js
@@ -0,0 +1,88 @@
+// 👍 10 String Tasks
+
+// Q1.----------- Length --------------->
+// Solve: =>
+// let str = "Ayush sharma";
+
+// console.log(str.length);
+
+// output: 1> 12
+
+
+// Q2.--------------- Uppercase --------->
+// Solve: =>
+// let str = "Javascript";
+
+// console.log(str.toUpperCase());
+
+//   output: 1> JAVASCRIPT
+
+// Q3.----------- Lowercase --------------->
+// solve: =>
+// let str = "javascript";
+// console.log(str.toLowerCase);
+
+//   OUTPUT: 1> Javascript
+
+// Q4.------------- charAt()--------------->
+// Solve: =>
+// let str = "ayush";
+// console.log(str.charAt(2));
+
+//  output: 1> u
+
+// Q5.--------------indexOf()-------------->
+// solve: =>
+// let str = "Hello world";
+// console.log(str.indexOf("world"));
+
+// output: 1> 6
+
+// Q6.------------- substring()------------>
+// Ṣolve: =>
+// let str = "javascript";
+// console.log(str.substring(0, 4));
+
+// output: 1> java
+
+// Q7. ------------- slice() -------------->
+// Solve:=>
+// let str = "programming";
+// console.log(str.slice(3, 7));
+
+// output: 1> gram
+
+// Q8.-------------- trim() -------------->
+// Solve: =>
+// let str = "   Ayush Sharma    ";
+// console.log(str.trim());
+
+// output: 1> Ayush Sharma
+
+// Q9.-------------- combine Strings ------->
+// Solve:=>
+// let firstName = "Ayush";
+// let lastName  = "sharma"
+
+// console.log(firstName + " " + lastName);
+
+// output 1> Ayush sharma
+
+// Q10.------------------- 🔥 Hard ------------>
+// Solve: =>
+// let str = "JavaScript Developer";
+
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.indexOf("Developer"));
+// console.log(str.slice(0, 10));
+// console.log(str.slice(11, 20));
+
+//  output: 1> 20
+//          2> JAVASCRIPT DEVELOPER
+//          3> javascript developer
+//          4> 11
+//          5> javascript
+//          6> Developer
+
