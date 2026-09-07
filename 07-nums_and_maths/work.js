598015d4ef6e8c6aa7936ee77536ebad4fa949eb
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-07 22:10:13 +0530

    Practice Math methods in javascript

---
+++ 07-nums_and_maths/work.js
@@ -0,0 +1,39 @@
+// JavaScript Numbers & Math — 10 Tasks :>
+
+// Q1.> Create a variable num = -25 and print its positive value using Math.abs() ?
+// solve: =>
+    // let num = -25;
+    // console.log(Math.abs(num));
+
+// output: 1> 25
+
+// Q2.> Create num = 7.8 and print the rounded value using Math.round()?
+// slove:==>
+    // let num = 7.8;
+    // console.log(Math.round(num));
+
+    // output: 1> 8
+
+// Q3.> Create num = 5.2 and print the smallest integer greater than or equal to it using Math.ceil() ?
+// Slove:=>
+    // let num = 5.2;
+    // console.log(Math.ceil(num));
+
+    // output: 1> 6
+
+// Q4. > Create num = 9.9 and print the largest integer less than or equal to it using Math.floor()?
+// Solve:=>
+    // let num = 9.9;
+    // console.log(Math.floor(num));
+
+    // output: 1> 9
+
+// Q5. > Generate a random number between 1 and 10 using Math.random() and Math.floor()?
+// Solve:=>
+    // let randomNumber = Math.floor(Math.random() * 10) + 1;
+    //  console.log(randomNumber);
+
+    //  output: 1 to 10
+
+
+      
