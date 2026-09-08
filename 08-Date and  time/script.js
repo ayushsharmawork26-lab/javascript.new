0044855908cff347b90c21bc3ae23f4d0e417bab
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-08 08:33:48 +0530

    javascript Date and Timestamp methods

--- 08-Date and  time/script.js
+++ 08-Date and  time/script.js
@@ -1,9 +1,19 @@
 // Dates
 
-let myDate = new Date();
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
-console.log(typeof myDate);
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
 
 
+// let myCreateDate = new Date(2023, 0, 23)
+// let myCreateDate = new Date(2023, 0, 23, 5, 3)
+// let myCreateDate = new Date("2026-09-08")
+let myCreateDate = new Date("01-14-2023")
+// console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+console.log(Math.floor(Date.now()/100));
