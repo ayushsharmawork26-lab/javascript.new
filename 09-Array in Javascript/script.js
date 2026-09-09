a8c6022f2cc43f07942ea9e39a64c72713bc90d3
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-09 08:43:34 +0530

    Array methods

--- 09-Array in Javascript/script.js
+++ 09-Array in Javascript/script.js
@@ -3,3 +3,13 @@ const myHeros = ["shaktiman", "naagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
 console.log(myArr[1]);
+
+//  +++++++++++++Array methods+++++++++++++++ ////
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+myArr.shift()
+console.log(myArr);
