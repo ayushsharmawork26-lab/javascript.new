88a5ff420c0e7d3e24d582bfead62e642cb69e30
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-10 08:38:38 +0530

    javascript array practice

---
+++ 09-Array in Javascript/work.js
@@ -0,0 +1,19 @@
+// JavaScript Array — 10 Practice Tasks:=>
+// Q1. Create an array of 5 numbers and print all the elements using console.log()?
+// Slove:=>
+
+    // let numbers = [10, 20, 30, 40, 50];
+
+    // console.log(numbers);
+
+    // Output: 1> [10, 20, 30, 40, 50]
+
+// Q2. Create an array of fruits: ["Apple", "Banana", "Mango", "Orange"]. Print the first and last element ?
+// Solve:=>
+
+    // let fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+    // console.log(fruits[0]);
+    // console.log(fruits[fruits.length - 1]);
+
+    // output: 1> 5
