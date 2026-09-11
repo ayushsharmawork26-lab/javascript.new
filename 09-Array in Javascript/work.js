beaa1dad85215bf382c0172bf7a4472d342552be
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-11 09:01:38 +0530

    use flat in array

--- 09-Array in Javascript/work.js
+++ 09-Array in Javascript/work.js
@@ -118,3 +118,12 @@ const dc_heros = ["superman", "flash", "batman"]
 const all_new_heros = [...marvel_heros, ...dc_heros]
 
 // console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6,7, [4,5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+console.log(Array.isArray("Ayush"))
+console.log(Array.from("Ayush"));
+console.log(Array.from({name: "ayush"}))       ///////   Interesting
