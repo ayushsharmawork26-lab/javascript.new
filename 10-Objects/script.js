ef9996cc76c47934ae8f5a39afe560d1e63ca97e
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-12 17:30:10 +0530

    object

---
+++ 10-Objects/script.js
@@ -0,0 +1,18 @@
+// singleton
+// Object.create
+
+// onject literals
+
+const jsUser = {
+    name: "Ayush",
+    "full name": "Ayush sharma",
+    age:  18,
+    location: "Chnadighar",
+    email: "ayushsharma@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(jsUser.email)
+console.log(jsUser["email"])
+console.log(jsUser["full name"])
