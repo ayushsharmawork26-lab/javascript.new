edc4ab52a9872946d9fd4fe5688e2c3e89dcc586
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-13 18:40:52 +0530

    creat a object properties

---
+++ 10-Objects/work.js
@@ -0,0 +1,35 @@
+// JavaScript Objects — 10 Practice Tasks
+// Q1. Create an object
+// Create a student object with properties:
+// . name
+// . age
+// . course
+// . city
+// solve:=>
+//     const student = {
+//         name: "Ayush",
+//         age:  23,
+//         course: "MCA",
+//         city:  "Chandighar"
+//     };
+
+// console.log(student)
+
+// output: 1> {name: 'Ayush', age: 23, course: 'MCA', city: 'Chandighar'}
+
+// Q2. Access object properties
+// >Create an object user with name, email, and age. Print:
+// > name using dot notation
+// > email using bracket notation
+// Solve:=>
+//     const user = {
+//     name: "Ayush",
+//     email: "ayush@gmail.com",
+//     age: 23
+// };
+
+// console.log(user.name);
+// console.log(user["email"]);
+
+// output: 1> Ayush
+//         2> ayush@gmail.com
