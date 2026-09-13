ef3fae1821249795d9401ed24fd49030ec9b4bbf
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-13 18:24:23 +0530

    greeting

--- 10-Objects/script.js
+++ 10-Objects/script.js
@@ -1,27 +1,38 @@
-// singleton
-// Object.create
+// // singleton
+// // Object.create
 
-// onject literals
+// // onject literals
 
-const mySym = Symbol("Key1")
+// const mySym = Symbol("Key1")
 
-const jsUser = {
-    name: "Ayush",
-    "full name": "Ayush sharma",
-    mySym: "myKey1",
-    age:  18,
-    location: "Chnadighar",
-    email: "ayushsharma@gmail.com",
-    isLoggedIn: false,
-    lastLoginDays: ["Monday", "Saturday"]
-}
+// const jsUser = {
+//     name: "Ayush",
+//     "full name": "Ayush sharma",
+//     mySym: "myKey1",
+//     age:  18,
+//     location: "Chnadighar",
+//     email: "ayushsharma@gmail.com",
+//     isLoggedIn: false,
+//     lastLoginDays: ["Monday", "Saturday"]
+// }
 
-console.log(jsUser.email)
-console.log(jsUser["email"])
-console.log(jsUser["full name"])
-console.log(typeof jsUser.mySym)
+// // console.log(jsUser.email)
+// // console.log(jsUser["email"])
+// // console.log(jsUser["full name"])
+// // console.log(typeof jsUser.mySym)
 
-jsUser.email = "ayush@chatgpt.com"
-Object.freeze(jsUser)
-jsUser.email = "ayush@ceosoft.com"
-console.log(jsUser)
+// jsUser.email = "ayush@chatgpt.com"
+// // Object.freeze(jsUser)
+// jsUser.email = "ayush@ceosoft.com"
+// // console.log(jsUser)
+
+// jsUser.greeting = function(){
+//     console.log("hello js user ");
+// }
+
+// jsUser.greetingTwo = function(){
+//     console.log(`Hello JS user, ${this.name}`);
+// }
+
+// console.log(jsUser.greeting());
+// console.log(jsUser.greetingTwo());
