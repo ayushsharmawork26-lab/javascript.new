b7f4dfb95677fe962bda7760779f2c2af38d9b6b
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-13 18:57:53 +0530

    object property

--- 10-Objects/work.js
+++ 10-Objects/work.js
@@ -33,3 +33,31 @@
 
 // output: 1> Ayush
 //         2> ayush@gmail.com
+
+// Q3. Update object property
+// > Create a car object with brand, model, and price. Change the price and print the updated object.
+// Solve:=>
+    // const car = {
+    //     brand: "Toyota",
+    //     model: "Fortuner",
+    //     price: 350000
+    // };
+
+    // car.price = 40000000;
+
+    // console.log(car);
+
+    // output: 1> brand: 'Toyota', model: 'Fortuner', price: 40000000}
+
+// Q4. Add a new property
+// > Create a person object with name and age. Add a new property job and print the object.
+// Solve:=>
+    // const person = {
+    //     name: "Ayush",
+    //     age:  23
+    // };
+
+    // person.job = "Full stack Developer";
+    // console.log(person);
+
+// output: 1>  {name: 'Ayush', age: 23, job: 'Full stack Developer'}
