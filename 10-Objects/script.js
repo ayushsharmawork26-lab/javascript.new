82a339dcc0d0b44cff8ccfd9ce3d1b1beadb9c88
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-14 18:48:48 +0530

    obj.assign

--- 10-Objects/script.js
+++ 10-Objects/script.js
@@ -60,3 +60,15 @@ const regularuser = {
 
 // console.log(regularuser.fullname.userfullname.firstname);
 
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2)
+
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+
