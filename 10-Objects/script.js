d57c013e174515c8078e8fae6fca148ac0e789f6
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-14 19:01:49 +0530

    use for entries and hasOwnProerty

--- 10-Objects/script.js
+++ 10-Objects/script.js
@@ -87,7 +87,9 @@ console.log(tinderUser);
 
 console.log(Object.keys(tinderUser));
 console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
 
+console.log(tinderUser.hasOwnProperty('isLoggesIn'));
        
 
 
