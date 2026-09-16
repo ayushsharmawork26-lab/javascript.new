ff69203d34b52b28bdd6dbdafc1bd22182ffc628
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-16 21:06:40 +0530

    result and return

--- 11-Functions & parameter/script.js
+++ 11-Functions & parameter/script.js
@@ -12,8 +12,18 @@ function sayMyNmae() {
 
 // sayMyNmae()
 
+// function addTwoNumbers(number1, Number2){
+//     console.log(number1 + Number2);
+// }
+
 function addTwoNumbers(number1, Number2){
-    console.log(number1 + Number2);
+   
+    let result = number1 + Number2
+     console.log("Ayush");
+    return result
+   
 }
 
-addTwoNumbers(3, 4)
+const result = addTwoNumbers(3, 5)
+
+console.log("result: ", result);
