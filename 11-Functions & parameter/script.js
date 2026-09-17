2246fe9db96647edcca9c711d862542962ebf7d8
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-17 20:01:47 +0530

    print the function greet and parameter

--- 11-Functions & parameter/script.js
+++ 11-Functions & parameter/script.js
@@ -1,40 +1,40 @@
 
 
 
-function sayMyNmae() {
-    console.log("A");
-    console.log("Y");
-    console.log("U");
-    console.log("S");
-    console.log("H");
+// function sayMyNmae() {
+//     console.log("A");
+//     console.log("Y");
+//     console.log("U");
+//     console.log("S");
+//     console.log("H");
 
-}
-
-// sayMyNmae()
-
-// function addTwoNumbers(number1, Number2){
-//     console.log(number1 + Number2);
 // }
 
-function addTwoNumbers(number1, Number2){
+// // sayMyNmae()
+
+// // function addTwoNumbers(number1, Number2){
+// //     console.log(number1 + Number2);
+// // }
+
+// function addTwoNumbers(number1, Number2){
    
-    let result = number1 + Number2
-    //  console.log("Ayush");
-    return result
+//     let result = number1 + Number2
+//     //  console.log("Ayush");
+//     return result
    
-}
+// }
 
-const result = addTwoNumbers(3, 5)
+// const result = addTwoNumbers(3, 5)
 
-// console.log("result: ", result);
+// // console.log("result: ", result);
 
-function loginUsermessage(username){
-    if(username === undefined){
-       console.log("Please entry a username")
-       return
-    }
-    return `${username} just logged in`
-} 
+// function loginUsermessage(username){
+//     if(username === undefined){
+//        console.log("Please entry a username")
+//        return
+//     }
+//     return `${username} just logged in`
+// } 
 
-// console.log(loginUsermessage("Ayush"))
-console.log(loginUsermessage());
+// // console.log(loginUsermessage("Ayush"))
+// console.log(loginUsermessage());
---
+++ 11-Functions & parameter/work.js
@@ -0,0 +1,20 @@
+// JavaScript Functions & Parameters — 10 Tasks
+// Q1. Create a function greet() that prints "Hello JavaScript" ?
+// Solve:=>
+    // function greet() {
+    //     console.log("Hello JavaScript");
+    // }
+
+    // greet();
+
+// output: 1> Hello JavaScript
+
+// Q2. Create a function welcome(name) that accepts a name as a parameter and prints "Welcome, Ayush" ?
+// Solve:=>
+    // function welcome(name) {
+    //     console.log(`Welcome, ${name}`);
+    // }
+
+    // welcome("Ayush");
+
+// Output: 1> Welcome, Ayush
