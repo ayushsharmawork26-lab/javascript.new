806ee937e543eff74225807427d43cbb6100dd25
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-17 20:30:48 +0530

    function add & subtract

--- 11-Functions & parameter/work.js
+++ 11-Functions & parameter/work.js
@@ -18,3 +18,23 @@
     // welcome("Ayush");
 
 // Output: 1> Welcome, Ayush
+
+// Q3. Create a function add(a, b) that takes two parameters and prints their sum ?
+// Solve:=>
+    // function add(a, b) {
+    //     console.log(a + b);
+
+    // }
+    // add(10, 20);
+
+// Output: 1> 30
+
+// Q4. Create a function subtract(a, b) that returns the difference of two numbers?
+// Solve:=>
+    // function subtract(a, b) {
+    //     return a - b;
+    // }
+   
+    // console.log(subtract(20, 8));
+
+// output: 12
