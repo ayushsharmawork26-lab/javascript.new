0dc6dd1200f2e227854b3eb0a5c6ea69a167429d
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-19 19:14:27 +0530

    return getArray

--- 11-Functions & parameter/script.js
+++ 11-Functions & parameter/script.js
@@ -64,3 +64,14 @@ handleObjects({
     username: "sam",
     price: 399
 })
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+    return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
