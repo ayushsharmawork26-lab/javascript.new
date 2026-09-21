167720d646ed42bfb9f90c7a4e297c5a6d96f3f5
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-21 21:14:32 +0530

    use for global & local function

--- 12-Global  & Local Scope/work.js
+++ 12-Global  & Local Scope/work.js
@@ -1,1 +1,27 @@
+// JavaScript Global & Local Scope — 10 Tasks
+// Q1. Create a global variable name = "Ayush" and print it inside a function ?
+// Solve:=>
+    // let name = "Ayush";
+    
+    // function showName() {
+    //     console.log(name);
+    // }
 
+    // showName();
+
+// output: 1> Ayush
+
+// Q2. Create a local variable age = 23 inside a function and try to print it outside the function. What happens?
+// Solve:=>
+    // function showAge() {
+    //     let age = 23;
+    //     console.log(age);
+    // }
+
+    // showAge();
+
+    // console.log(age);
+
+// Output:
+// 23
+// ReferenceError: age is not defined
