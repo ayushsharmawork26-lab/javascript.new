e54a6cf2e78dec3395a135c1deff7ba1d6186d07
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-22 22:29:36 +0530

    scope level

--- 12-Global  & Local Scope/script.js
+++ 12-Global  & Local Scope/script.js
@@ -10,3 +10,30 @@
 // console.log(a);
 // // console.log(b);
 // console.log(c);
+
+// +++++++++++++++++++( Scope level in javascript )++++++++++++++++++
+
+function one(){
+    const username = "Ayush"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "Ayush"
+    if (username === "Ayush") {
+       const website = " youtube" 
+    //    console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
