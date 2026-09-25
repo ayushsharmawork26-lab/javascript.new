023490c4ebe84777aeac45222a4389bdc09b8013
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-25 21:13:26 +0530

    use function

--- 13-THIS and arrow function/script.js
+++ 13-THIS and arrow function/script.js
@@ -4,9 +4,19 @@ const user = {
 
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);
+        console.log(this);
     }
 }
 
-user.welcomeMessage()
-user.username = "sam"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function one(){
+//     let username = "Ayush"
+//     console.log(this.username);
+// }
+
+// one()
