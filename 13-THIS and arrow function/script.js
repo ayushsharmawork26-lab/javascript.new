66fe51a7ecdc3546fb5b13aed388e3838465c8fb
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-25 21:40:45 +0530

    use aroow function

--- 13-THIS and arrow function/script.js
+++ 13-THIS and arrow function/script.js
@@ -1,34 +1,49 @@
-const user = {
-    username: "Ayush",
-    price: 999,
+// const user = {
+//     username: "Ayush",
+//     price: 999,
 
-    welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
-    }
-}
-
-// user.welcomeMessage()
-// user.username = "sam"
-// user.welcomeMessage()
-
-// console.log(this);
-
-// function one(){
-//     let username = "Ayush"
-//     console.log(this.username);
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`);
+//         console.log(this);
+//     }
 // }
 
-// one()
+// // user.welcomeMessage()
+// // user.username = "sam"
+// // user.welcomeMessage()
 
-// const one = function () {
+// // console.log(this);
+
+// // function one(){
+// //     let username = "Ayush"
+// //     console.log(this.username);
+// // }
+
+// // one()
+
+// // const one = function () {
+// //     let username = "Ayush"
+// //     console.log(this.username);
+// // }
+
+// const one =  () => {
 //     let username = "Ayush"
-//     console.log(this.username);
+//     console.log(this);
 // }
 
-const one =  () => {
-    let username = "Ayush"
-    console.log(this);
-}
+// // one()
 
-// one()
+// // const addTwo = (num1, num2) => {
+// //     return num1 + num2
+// // }
+
+// // const addTwo = (num1, num2) =>  num1 + num2
+
+// // const addTwo = (num1, num2) => ( num1 + num2 )
+
+// const addTwo = (num1, num2) => ( {username: "Ayush"} )
+// console.log(addTwo(3, 4))
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
