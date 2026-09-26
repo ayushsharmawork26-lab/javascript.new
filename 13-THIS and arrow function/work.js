53db0460b29880a8e91a900bc96ce394134d25e8
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-26 17:25:27 +0530

    use Arrow function and Addition

---
+++ 13-THIS and arrow function/work.js
@@ -0,0 +1,23 @@
+// JavaScript Arrow Function — 10 Practice Tasks
+
+// Q1. Simple Arrow Function
+// Create an arrow function greet that prints "Hello Ayush".
+// Solve:=>
+    // const greet = () => {
+    //     console.log("Hello Ayush");
+    // };
+
+    // greet();
+
+// Output: 1> Hello Ayush
+
+// Q2. Addition
+// Create an arrow function add that takes two numbers and returns their sum.
+// Solve:=>
+    // const add = (a, b) => {
+    //     return a + b;
+    // };
+
+    // console.log(add(10, 20));
+
+// Output:  1> 30
