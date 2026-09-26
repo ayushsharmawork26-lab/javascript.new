1efaa82fff417b9564b137784a5b2229892fdfb0
ayushsharma.work26@gmail.com <ayushsharma.work26@gmail.com>
2026-09-26 17:39:08 +0530

    use arrow function and square and checkEven

--- 13-THIS and arrow function/work.js
+++ 13-THIS and arrow function/work.js
@@ -21,3 +21,29 @@
     // console.log(add(10, 20));
 
 // Output:  1> 30
+
+// Q3. Square
+// Create an arrow function square that takes a number and returns its square.
+// Solve:=>
+    // const square = (num) => {
+    //     return num * num;
+    // };
+
+    // console.log(square(5));
+
+// Output: 1> 25
+
+// Q4. Even or Odd
+// Create an arrow function checkEven that takes a number and returns "Even" if the number is even, otherwise "Odd".
+// solve:=>
+    // const checkEven = (num) => {
+    //     if (num % 2 === 0) {
+    //         return "Even";
+    //     } else{
+    //         return "Odd";
+    //     }
+    // };
+
+    // console.log(checkEven(7));
+
+// Output: 1> Odd
